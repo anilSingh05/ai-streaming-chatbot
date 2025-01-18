8ee9549820461f273f345a6b1427aeb4d5b61373
Anil Singh <anil05.me@gmail.com>
2025-01-17 20:55:42 -0600

    Initial commit

---
+++ src/main/java/ai_streaming_chatbot/AiStreamingChatbotApplication.java
@@ -0,0 +1,13 @@
+package ai_streaming_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStreamingChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiStreamingChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai_streaming_chatbot/AiStreamingChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ai_streaming_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStreamingChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
